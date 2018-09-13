366982a4d6464fe4a2afb179a6e136c82ffcb25c
Kawarimi <101errorsmore@gmail.com>
2018-09-13 10:58:14 +0300

    First Commit-Basic main menu

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void GoToGithub()
+	{
+    Process.Start("https://github.com/Kawarimi/openstone");
+	}
+	void ChangeToMatch()
+	{
+    SceneManager.LoadScene("Game");
+	}
+	void ChangeToDecks()
+	{
+    SceneManager.LoadScene("DeckManager");
+	}
+}
